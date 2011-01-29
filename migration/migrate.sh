06f68b2d92d5bcc2fe923378e51a4b314556d888
Damien Tournoud <damien@commerceguys.com>
2011-01-29 17:01:09 -0500

    Fix the paths.

--- migration/migrate.sh
+++ migration/migrate.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Needs to be called from www7.
+
 # Clean-up the database.
-drush sqlc < sql/pre-import.sql
+drush sqlc < ../migration/sql/pre-import.sql
 
 # Import the D6 database.
 drush sqlc < ../export/drupalfr-live-sanitized.sql
 
 # Fix some of the properties of the D6 database.
-drush sqlc < sql/pre-migration.sql
+drush sqlc < ../migration/sql/pre-migration.sql
 
 # Try a migration.
-cd ../www7
 drush update-db -y
+
+
