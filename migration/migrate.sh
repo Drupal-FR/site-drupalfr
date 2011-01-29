716abba3188f4e089323e1beae7917d0e540e5a2
Damien Tournoud <damien@commerceguys.com>
2011-01-29 17:36:02 -0500

    Speed-up the upgrade process by skipping password hashing and defaulting to MyISAM.

--- migration/migrate.sh
+++ migration/migrate.sh
@@ -7,7 +7,7 @@ set -xe
 drush sqlc < ../migration/sql/pre-import.sql
 
 # Import the D6 database.
-drush sqlc < ../export/drupalfr-live-sanitized.sql
+cat ../export/drupalfr-live-sanitized.sql | sed -e 's/^) ENGINE=InnoDB/) ENGINE=MyISAM/' | drush sqlc
 
 # Fix some of the properties of the D6 database.
 drush sqlc < ../migration/sql/pre-migration.sql
