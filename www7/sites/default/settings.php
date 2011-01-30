2572413c2308862f94503af2ec1768c8ced1a566
Nicolas Meyer <meyer.nicolas@gmail.com>
2011-01-30 12:15:42 +0100

    create the settings.php, with link to settings.local.php

---
+++ www7/sites/default/settings.php
@@ -0,0 +1,33 @@
+<?php
+
+
+/**
+ * Database settings:
+ */
+$databases = array();
+
+/**
+ * Access control for update.php script.
+ */
+$update_free_access = FALSE;
+
+/**
+ * Salt for one-time login links and cancel links, form tokens, etc.
+ */
+$drupal_hash_salt = '';
+
+/**
+ * PHP settings:
+ */
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+ini_set('session.gc_maxlifetime', 200000);
+ini_set('session.cookie_lifetime', 2000000);
+# ini_set('pcre.backtrack_limit', 200000);
+# ini_set('pcre.recursion_limit', 200000);
+
+/**
+ * Local settings (databases password, domain configuration, ...).
+ */
+require dirname(__FILE__) . '/settings.local.php';
+
