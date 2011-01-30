c26be84c43a168d27c59189b029bd6a1cda50c43
Nicolas Meyer <meyer.nicolas@gmail.com>
2011-01-30 12:33:13 +0100

    add pathauto module

---
+++ www7/sites/all/modules/contrib/pathauto/pathauto.api.php
@@ -0,0 +1,19 @@
+<?php
+// $Id: pathauto.api.php,v 1.2 2010/08/08 23:46:36 davereid Exp $
+
+/**
+ * @file
+ * Documentation for pathauto API.
+ *
+ * @see hook_token_info
+ * @see hook_tokens
+ */
+
+function hook_path_alias_types() {
+}
+
+function hook_pathauto($op) {
+}
+
+function hook_pathauto_alias_alter(&$alias, array $context) {
+}
