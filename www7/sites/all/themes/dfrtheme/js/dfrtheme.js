9024654cbbdf2989081456c3114f1a118ff979d2
Olivier PIERRE <oli.pierre@gmail.com>
2012-12-16 11:17:25 +0100

    Fix bug in dfrtheme.js

--- www7/sites/all/themes/dfrtheme/js/dfrtheme.js
+++ www7/sites/all/themes/dfrtheme/js/dfrtheme.js
@@ -16,7 +16,7 @@
     $('a').each(function() {
      var a = new RegExp('/' + window.location.host + '/');
      if(!a.test(this.href)) {
-         a.attr('target', '_blank');
+         $(this).attr('target', '_blank');
       }
     });
   } 
