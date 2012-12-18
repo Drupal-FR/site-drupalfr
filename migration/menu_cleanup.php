300110170d1f502d6029c4a82142d0966264a3f6
Artusamak <julien@commerceguys.com>
2012-12-19 00:05:21 +0100

    Fix SQL query fetching Ressources menu entry mlid.

--- migration/menu_cleanup.php
+++ migration/menu_cleanup.php
@@ -5,7 +5,7 @@ _menu_delete_item(menu_link_load(1209), 1);
 _menu_delete_item(menu_link_load(1954), 1);
 
 // Move the ressources link in the navigation menu.
-$ressouces_mlid = db_query("SELECT mlid FROM menu_links WHERE router_path = 'Ressources'")->fetchAssoc();
+$ressouces_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Ressources'")->fetchAssoc();
 $item = menu_link_load($ressouces_mlid["mlid"]);
 $item["menu_name"] = "navigation";
 $item["p1"] = $ressouces_mlid["mlid"];
