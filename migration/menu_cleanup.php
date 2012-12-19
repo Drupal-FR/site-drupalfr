ef1c0ec5bc3f27b4434c240b930e02e2f468d5d7
Artusamak <julien@commerceguys.com>
2012-12-19 17:58:15 +0100

    Restore menu deletion.

--- migration/menu_cleanup.php
+++ migration/menu_cleanup.php
@@ -1,12 +1,14 @@
 <?php
 
 // Remove the fantom link about user listing and unwanted links.
-// _menu_delete_item(menu_link_load(1209), 1);
-// _menu_delete_item(menu_link_load(1954), 1);
+_menu_delete_item(menu_link_load(1209), 1);
+_menu_delete_item(menu_link_load(1954), 1);
 
 // Move the ressources link in the navigation menu.
-// $ressouces_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Ressources'")->fetchAssoc();
-// $item = menu_link_load($ressouces_mlid["mlid"]);
-// $item["menu_name"] = "navigation";
-// $item["p1"] = $ressouces_mlid["mlid"];
-// menu_link_save($item);
+$ressources_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Ressources'")->fetchAssoc();
+if ($ressources_mlid) {
+  $item = menu_link_load($ressources_mlid["mlid"]);
+  $item["menu_name"] = "navigation";
+  $item["p1"] = $ressources_mlid["mlid"];
+  menu_link_save($item);
+}
