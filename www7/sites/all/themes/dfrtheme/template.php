221055e51bf76943d071e2fd96fa1ec823308053
Simon Georges <simon.georges@makina-corpus.com>
2013-01-20 14:49:49 +0100

    Add &nbsp; before ':' (special typograhic gift to Pomliane)

--- www7/sites/all/themes/dfrtheme/template.php
+++ www7/sites/all/themes/dfrtheme/template.php
@@ -326,5 +326,25 @@ function dfrtheme_form_alter(&$form, &$form_state, $form_id) {
   }
 } // dfrtheme_form_alter
 
+function dfrtheme_field($variables) {
+  $output = '';
 
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . '&nbsp;:&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
 
