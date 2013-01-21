cd01eb3b92fd1c8d26e83957c7ce826e50864a5c
Artusamak <julien@commerceguys.com>
2013-01-21 17:47:04 +0100

    Clean menus and update the source dump.

--- migration/menu_cleanup.php
+++ migration/menu_cleanup.php
@@ -4,6 +4,11 @@
 _menu_delete_item(menu_link_load(1209), 1);
 _menu_delete_item(menu_link_load(1954), 1);
 
+// Remove the emploi link from the main menu.
+$result = db_query("SELECT mlid FROM menu_links WHERE menu_links.link_title = 'Emploi' AND menu_links.menu_name = 'main-menu'");
+$record = $result->fetch();
+_menu_delete_item(menu_link_load($record->mlid), 1);
+
 // Move the ressources link in the navigation menu.
 $ressources_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Ressources'")->fetchAssoc();
 if ($ressources_mlid) {
@@ -13,8 +18,35 @@ if ($ressources_mlid) {
   menu_link_save($item);
 }
 
+// Move the Drupalcamp link in the navigation menu.
+$drupalcamp_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Drupalcamp' AND menu_name = 'main-menu'")->fetchAssoc();
+if ($drupalcamp_mlid) {
+  $item = menu_link_load($drupalcamp_mlid["mlid"]);
+  $item["menu_name"] = "navigation";
+  $item["p1"] = $drupalcamp_mlid["mlid"];
+  menu_link_save($item);
+}
+
 // Reset some menu_items to have them translated
 menu_reset_item(menu_link_load(22));
 menu_reset_item(menu_link_load(39));
 menu_reset_item(menu_link_load(1213));
 menu_reset_item(menu_link_load(1214));
+
+// Resort the menu items in the navigation menu.
+// Logout link.
+$logout_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Log out' AND menu_name = 'navigation'")->fetchAssoc();
+if ($logout_mlid) {
+  $item = menu_link_load($logout_mlid["mlid"]);
+  $item["weight"] = 0;
+  menu_link_save($item);
+}
+
+// Content creation link.
+$content_mlid = db_query("SELECT mlid FROM menu_links WHERE link_path = 'node/add' AND menu_name = 'navigation';")->fetchAssoc();
+if ($content_mlid) {
+  $item = menu_link_load($content_mlid["mlid"]);
+  $item["weight"] = 1;
+  menu_link_save($item);
+}
+
