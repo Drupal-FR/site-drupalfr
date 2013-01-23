cd90276801e3bad275454cc79dfcf456ec703c72
Artusamak <julien@commerceguys.com>
2013-01-23 17:06:47 +0100

    Start menu cleanup for user section.

--- migration/menu_cleanup.php
+++ migration/menu_cleanup.php
@@ -17,7 +17,7 @@ foreach (array(457, 27, $planete_mlid['mlid'], 1125, 17) as $mlid) {
 $ressources_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Ressources'")->fetchAssoc();
 if ($ressources_mlid) {
   $item = menu_link_load($ressources_mlid["mlid"]);
-  $item["menu_name"] = "navigation";
+  $item["menu_name"] = "user-menu";
   $item["p1"] = $ressources_mlid["mlid"];
   menu_link_save($item);
 }
@@ -26,7 +26,7 @@ if ($ressources_mlid) {
 $drupalcamp_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Drupalcamp' AND menu_name = 'main-menu'")->fetchAssoc();
 if ($drupalcamp_mlid) {
   $item = menu_link_load($drupalcamp_mlid["mlid"]);
-  $item["menu_name"] = "navigation";
+  $item["menu_name"] = "user-menu";
   $item["p1"] = $drupalcamp_mlid["mlid"];
   menu_link_save($item);
 }
@@ -37,23 +37,87 @@ menu_reset_item(menu_link_load(39));
 menu_reset_item(menu_link_load(1213));
 menu_reset_item(menu_link_load(1214));
 
+// Reset some menu items from the navigation menu.
+// Recent message.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'navigation' AND link_title = 'Messages récents'")->fetchAssoc();
+menu_reset_item(menu_link_load($mlid['mlid']));
+
+// Recent posts.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'navigation' AND link_title = 'Recent posts'")->fetchAssoc();
+menu_reset_item(menu_link_load($mlid['mlid']));
+
+// Log out.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'navigation' AND link_title = 'Log out'")->fetchAssoc();
+menu_reset_item(menu_link_load($mlid['mlid']));
+
 // Clear the menu caches.
 menu_cache_clear_all();
 
-// Resort the menu items in the navigation menu.
-// Logout link.
-$logout_mlid = db_query("SELECT mlid FROM menu_links WHERE link_title = 'Log out' AND menu_name = 'navigation'")->fetchAssoc();
-if ($logout_mlid) {
-  $item = menu_link_load($logout_mlid["mlid"]);
-  $item["weight"] = 0;
+// Build the user menu for navigation.
+// 1. Log out.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'user-menu' AND link_title = 'Log out'")->fetchAssoc();
+if ($mlid) {
+  $item = menu_link_load($mlid["mlid"]);
+  $item["weight"] = -15;
+  $item["p1"] = $mlid["mlid"];
   menu_link_save($item);
 }
 
-// Content creation link.
-$content_mlid = db_query("SELECT mlid FROM menu_links WHERE link_path = 'node/add' AND menu_name = 'navigation';")->fetchAssoc();
-if ($content_mlid) {
-  $item = menu_link_load($content_mlid["mlid"]);
-  $item["weight"] = 1;
+// 2. My account.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'user-menu' AND link_title = 'User account'")->fetchAssoc();
+if ($mlid) {
+  $item = menu_link_load($mlid["mlid"]);
+  $item["weight"] = -14;
+  $item["p1"] = $mlid["mlid"];
   menu_link_save($item);
 }
 
+// 3. Drupalcamp.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'user-menu' AND link_title = 'Drupalcamp'")->fetchAssoc();
+if ($mlid) {
+  $item = menu_link_load($mlid["mlid"]);
+  $item["weight"] = -13;
+  $item["p1"] = $mlid["mlid"];
+  menu_link_save($item);
+}
+
+// 4. Ressources.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'user-menu' AND link_title = 'Ressources'")->fetchAssoc();
+if ($mlid) {
+  $item = menu_link_load($mlid["mlid"]);
+  $item["weight"] = -12;
+  $item["p1"] = $mlid["mlid"];
+  menu_link_save($item);
+}
+
+// 5. Add content.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'navigation' AND link_title = 'Add content'")->fetchAssoc();
+if ($mlid) {
+  $item = menu_link_load($mlid["mlid"]);
+  $item["weight"] = -11;
+  $item['menu_name'] = 'user-menu';
+  $item["p1"] = $mlid["mlid"];
+  menu_link_save($item);
+}
+
+// 6. Recent messages.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'navigation' AND link_title = 'Recent posts'")->fetchAssoc();
+if ($mlid) {
+  $item = menu_link_load($mlid["mlid"]);
+  $item["weight"] = -10;
+  $item['menu_name'] = 'user-menu';
+  $item["p1"] = $mlid["mlid"];
+  menu_link_save($item);
+}
+
+// 7. Job offers moderation.
+$mlid = db_query("SELECT mlid FROM menu_links WHERE menu_name = 'navigation' AND link_title = 'Modération des offres'")->fetchAssoc();
+if ($mlid) {
+  $item = menu_link_load($mlid["mlid"]);
+  $item["weight"] = -9;
+  $item['menu_name'] = 'user-menu';
+  $item["p1"] = $mlid["mlid"];
+  menu_link_save($item);
+}
+
+menu_cache_clear();
