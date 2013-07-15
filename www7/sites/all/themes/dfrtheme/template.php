0feab5a1fd6dc77bffe33045d7cfeec63443b499
Artusamak <julien@commerceguys.com>
2013-07-15 15:32:19 +0200

    Fix notice.

--- www7/sites/all/themes/dfrtheme/template.php
+++ www7/sites/all/themes/dfrtheme/template.php
@@ -253,7 +253,7 @@ function dfrtheme_id_safe($string) {
   // Replace with dashes anything that isn't A-Z, numbers, dashes, or underscores.
   $string = strtolower(preg_replace('/[^a-zA-Z0-9_-]+/', '-', $string));
   // If the first character is not a-z, add 'n' in front.
-  if (!ctype_lower($string{0})) { // Don't use ctype_alpha since its locale aware.
+  if (strlen($string) > 0 && !ctype_lower($string{0})) { // Don't use ctype_alpha since its locale aware.
     $string = 'id' . $string;
   }
   return $string;
