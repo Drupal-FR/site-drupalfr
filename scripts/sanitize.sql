ac8158c7a2ef807f6894724b5a761b711f0b342d
Edouard Cunibil <admin@duael.fr>
2014-01-24 22:29:23 +0100

    Fix sanitize.sql for D7 tables

--- scripts/sanitize.sql
+++ scripts/sanitize.sql
@@ -1,7 +1,7 @@
 
 -- Munge emails for security.
-UPDATE users SET mail = CONCAT(name, '@localhost'), init = CONCAT('http://drupalfr.org/user/', uid, '/edit'), pass = MD5(CONCAT('drupalfr', name)), login = 280299600, access = 280299600;
-UPDATE comments SET mail = CONCAT(name, '@localhost'), hostname = '';
+UPDATE users SET mail = CONCAT(name, '@localhost'), init = CONCAT('http://drupalfr.org/user/', uid, '/edit'), login = 280299600, access = 280299600;
+UPDATE comment SET mail = CONCAT(name, '@localhost'), hostname = '';
 UPDATE authmap SET authname = CONCAT(aid, '@localhost');
 UPDATE client SET mail = CONCAT(cid, '@localhost');
 UPDATE simplenews_subscriptions SET mail = CONCAT(snid, '@localhost');
@@ -12,6 +12,13 @@ UPDATE content_type_profile SET field_link_url = 'http://drupalfr.org' WHERE fie
 UPDATE content_type_profile SET field_msn_email = CONCAT(nid, '@localhost') WHERE field_msn_email IS NOT NULL;
 UPDATE content_type_profile SET field_realname_value = 'REAL NAME' WHERE field_realname_value IS NOT NULL;
 
+UPDATE field_data_field_mail SET field_mail_email = CONCAT(entity_id, '@localhost');
+UPDATE field_data_field_icq SET field_icq_value = entity_id WHERE field_icq_value IS NOT NULL;
+UPDATE field_data_field_jabber SET field_jabber_value = entity_id WHERE field_jabber_value IS NOT NULL;
+UPDATE field_data_field_link SET field_link_url = 'http://drupalfr.org' WHERE field_link_url IS NOT NULL;
+UPDATE field_data_field_msn SET field_msn_email = CONCAT(entity_id, '@localhost') WHERE field_msn_email IS NOT NULL;
+UPDATE field_data_field_realname SET field_realname_value = 'REAL NAME' WHERE field_realname_value IS NOT NULL;
+
 -- Remove sensitive variables
 DELETE FROM variable WHERE name = 'drupal_private_key';
 DELETE FROM variable WHERE name LIKE '%key%';
@@ -19,11 +26,10 @@ DELETE FROM variable WHERE name = 'cron_semaphore';
 
 -- Get rid of unpublished/blocked nodes, users, comments and related data in other tables.
 DELETE FROM node WHERE status <> 1;
-DELETE FROM comments WHERE status <> 0;
+DELETE FROM comment WHERE status <> 0;
 DELETE FROM users WHERE status <> 1 AND uid <> 0;
 DELETE node FROM node LEFT JOIN users ON node.uid = users.uid WHERE users.uid IS NULL;
-DELETE node_revisions FROM node_revisions LEFT JOIN node ON node.nid = node_revisions.nid WHERE node.nid IS NULL;
-DELETE comments FROM comments LEFT JOIN node ON node.nid = comments.nid WHERE node.nid IS NULL;
-DELETE comments FROM comments LEFT JOIN users ON comments.uid = users.uid WHERE users.uid IS NULL;
-DELETE comments FROM comments LEFT JOIN comments c2 ON comments.pid = c2.cid WHERE c2.cid IS NULL AND comments.pid <> 0;
-
+DELETE node_revision FROM node_revision LEFT JOIN node ON node.nid = node_revision.nid WHERE node.nid IS NULL;
+DELETE comment FROM comment LEFT JOIN node ON node.nid = comment.nid WHERE node.nid IS NULL;
+DELETE comment FROM comment LEFT JOIN users ON comment.uid = users.uid WHERE users.uid IS NULL;
+DELETE comment FROM comment LEFT JOIN comment c2 ON comment.pid = c2.cid WHERE c2.cid IS NULL AND comment.pid <> 0;
