c33dac0dbd4ba2a959a184bf2d007b3c0b761201
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-05-10 15:36:27 +0200

    Issue #519: Sanitize commerce customer address, biography nickname and the revisions.

--- scripts/sanitize.sql
+++ scripts/sanitize.sql
@@ -1,6 +1,6 @@
 
 -- Munge emails for security.
-UPDATE users                    SET mail = CONCAT(name,'@localhost.local'), init = CONCAT('http://drupalfr.org/user/', uid, '/edit'), pass = '', login = 280299600, access = 280299600;
+UPDATE users                    SET mail = CONCAT(name, '@localhost.local'), init = CONCAT('http://drupalfr.org/user/', uid, '/edit'), pass = '', login = 280299600, access = 280299600;
 UPDATE comment                  SET mail = CONCAT(name, '@localhost.local'), hostname = '';
 UPDATE authmap                  SET authname             = CONCAT(aid, '@localhost.local');
 UPDATE client                   SET mail                 = CONCAT(cid, '@localhost.local');
@@ -12,12 +12,59 @@ UPDATE content_type_profile     SET field_jabber_value   = nid                  
 UPDATE content_type_profile     SET field_link_url       = 'http://drupalfr.org'           WHERE field_link_url       IS NOT NULL;
 UPDATE content_type_profile     SET field_realname_value = 'REAL NAME'                     WHERE field_realname_value IS NOT NULL;
 
-UPDATE field_data_field_msn      SET field_msn_email      = CONCAT(entity_id, '@localhost.local') WHERE field_msn_email      IS NOT NULL;
-UPDATE field_data_field_mail     SET field_mail_email     = CONCAT(entity_id, '@localhost.local') WHERE field_mail_email     IS NOT NULL;
-UPDATE field_data_field_icq      SET field_icq_value      = entity_id                             WHERE field_icq_value      IS NOT NULL;
-UPDATE field_data_field_jabber   SET field_jabber_value   = entity_id                             WHERE field_jabber_value   IS NOT NULL;
-UPDATE field_data_field_link     SET field_link_url       = 'http://drupalfr.org'                 WHERE field_link_url       IS NOT NULL;
-UPDATE field_data_field_realname SET field_realname_value = 'REAL NAME'                           WHERE field_realname_value IS NOT NULL;
+-- User fields.
+UPDATE field_data_field_msn       SET field_msn_email       = CONCAT(entity_id, '@localhost.local') WHERE field_msn_email       IS NOT NULL;
+UPDATE field_data_field_mail      SET field_mail_email      = CONCAT(entity_id, '@localhost.local') WHERE field_mail_email      IS NOT NULL;
+UPDATE field_data_field_icq       SET field_icq_value       = entity_id                             WHERE field_icq_value       IS NOT NULL;
+UPDATE field_data_field_jabber    SET field_jabber_value    = entity_id                             WHERE field_jabber_value    IS NOT NULL;
+UPDATE field_data_field_link      SET field_link_url        = 'http://drupalfr.org'                 WHERE field_link_url        IS NOT NULL;
+UPDATE field_data_field_realname  SET field_realname_value  = 'REAL NAME'                           WHERE field_realname_value  IS NOT NULL;
+UPDATE field_data_field_nickname  SET field_nickname_value  = 'NICK NAME'                           WHERE field_nickname_value  IS NOT NULL;
+UPDATE field_data_field_biography SET field_biography_value = 'Biography'                           WHERE field_biography_value IS NOT NULL;
+-- field_planete_rss?
+-- field_membership_number?
+
+-- User field revisions.
+UPDATE field_revision_field_msn       SET field_msn_email       = CONCAT(entity_id, '@localhost.local') WHERE field_msn_email       IS NOT NULL;
+UPDATE field_revision_field_mail      SET field_mail_email      = CONCAT(entity_id, '@localhost.local') WHERE field_mail_email      IS NOT NULL;
+UPDATE field_revision_field_icq       SET field_icq_value       = entity_id                             WHERE field_icq_value       IS NOT NULL;
+UPDATE field_revision_field_jabber    SET field_jabber_value    = entity_id                             WHERE field_jabber_value    IS NOT NULL;
+UPDATE field_revision_field_link      SET field_link_url        = 'http://drupalfr.org'                 WHERE field_link_url        IS NOT NULL;
+UPDATE field_revision_field_realname  SET field_realname_value  = 'REAL NAME'                           WHERE field_realname_value  IS NOT NULL;
+UPDATE field_revision_field_nickname  SET field_nickname_value  = 'NICK NAME'                           WHERE field_nickname_value  IS NOT NULL;
+UPDATE field_revision_field_biography SET field_biography_value = 'Biography'                           WHERE field_biography_value IS NOT NULL;
+
+-- User commerce address.
+UPDATE field_data_commerce_customer_address SET
+  commerce_customer_address_country = 'FR',
+  commerce_customer_address_administrative_area = '',
+  commerce_customer_address_sub_administrative_area = '',
+  commerce_customer_address_locality = 'Paris',
+  commerce_customer_address_dependent_locality = '',
+  commerce_customer_address_postal_code = '75003',
+  commerce_customer_address_thoroughfare = '21 Place de la République',
+  commerce_customer_address_premise = '',
+  commerce_customer_address_sub_premise = '',
+  commerce_customer_address_organisation_name = '',
+  commerce_customer_address_name_line = 'Association Drupal France et Francophonie',
+  commerce_customer_address_first_name = 'Association',
+  commerce_customer_address_last_name = 'Drupal France et Francophonie',
+  commerce_customer_address_data = '';
+UPDATE field_revision_commerce_customer_address SET
+  commerce_customer_address_country = 'FR',
+  commerce_customer_address_administrative_area = '',
+  commerce_customer_address_sub_administrative_area = '',
+  commerce_customer_address_locality = 'Paris',
+  commerce_customer_address_dependent_locality = '',
+  commerce_customer_address_postal_code = '75003',
+  commerce_customer_address_thoroughfare = '21 Place de la République',
+  commerce_customer_address_premise = '',
+  commerce_customer_address_sub_premise = '',
+  commerce_customer_address_organisation_name = '',
+  commerce_customer_address_name_line = 'Association Drupal France et Francophonie',
+  commerce_customer_address_first_name = 'Association',
+  commerce_customer_address_last_name = 'Drupal France et Francophonie',
+  commerce_customer_address_data = '';
 
 -- Remove sensitive variables.
 DELETE FROM variable WHERE name = 'drupal_private_key';
