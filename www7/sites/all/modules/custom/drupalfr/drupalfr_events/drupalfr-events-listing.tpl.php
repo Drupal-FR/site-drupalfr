45043b2b3ba613891214e54c5162009491846637
Florent Torregrosa <florent.torregrosa@gmail.com>
2015-11-22 17:21:18 +0100

    Issue #720, #702: Update meetup mapts.

--- www7/sites/all/modules/custom/drupalfr/drupalfr_events/drupalfr-events-listing.tpl.php
+++ www7/sites/all/modules/custom/drupalfr/drupalfr_events/drupalfr-events-listing.tpl.php
@@ -10,17 +10,23 @@
   <?php if ($display_mode == 'teaser'): ?>
     <p>
       <span class="event-name"><?php print l($event['name'], $event['event_url']); ?></span><br />
-      <span class="event-date"><?php print format_date($event['time'] / 1000, 'short'); ?></span> //
-      <span class="event-attendees"><?php print $event['yes_rsvp_count']; ?> participants</span>
+      <span class="event-date"><?php print format_date($event['time'] / 1000, 'medium_with_at'); ?> -</span>
+      <span class="event-attendees"><?php print format_plural($event['drupalfr_inscription_count'], $event['drupalfr_inscription_count'] . ' inscrit', $event['drupalfr_inscription_count'] . ' inscrits'); ?></span>
+      <?php if (isset($event['rsvp_limit'])): ?>
+        <span class="event-limit">sur <?php print $event['rsvp_limit']; ?> places</span>
+      <?php endif; ?>
     </p>
   <?php elseif ($display_mode == 'full'): ?>
       <h2 class="event-name"><?php print l($event['name'], $event['event_url']); ?></h2>
       <div class="event-extra-info">
-        <span class="event-date"><?php print format_date($event['time'] / 1000, 'short'); ?></span> //
-        <span class="event-attendees"><?php print $event['yes_rsvp_count']; ?> participants</span>
+        <span class="event-date"><?php print format_date($event['time'] / 1000, 'medium_with_at'); ?> -</span>
+        <span class="event-attendees"><?php print format_plural($event['drupalfr_inscription_count'], $event['drupalfr_inscription_count'] . ' inscrit', $event['drupalfr_inscription_count'] . ' inscrits'); ?></span>
+        <?php if (isset($event['rsvp_limit'])): ?>
+          <span class="event-limit">pour <?php print $event['rsvp_limit']; ?> places</span>
+        <?php endif; ?>
       </div>
       <?php if (isset($event['description'])): ?>
-        <div class="event-description"><?php print $event['description']; ?></div>
+        <div class="event-description"><?php print text_summary($event['description'], NULL, 500); ?></div>
       <?php endif; ?>
   <?php endif; ?>
   </div>
