8e5e56df6f151879ab6c153cedb2f464aedfb34f
Florent Torregrosa <florent.torregrosa@gmail.com>
2016-08-14 13:39:02 +0200

    Add geofields data in migrate imports.

---
+++ www/profiles/custom/drupalfr/modules/custom/drupalfr_migrate/src/Plugin/migrate/process/MultiGeoFields.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\drupalfr_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Prepare an array of geoPHP object in wkt.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "drupalfr_multigeofields"
+ * )
+ */
+class MultiGeoFields extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (empty($this->configuration['source'])) {
+      throw new MigrateException('source is empty');
+    }
+
+    if (empty($this->configuration['delimiter'])) {
+      throw new MigrateException('delimiter is empty');
+    }
+
+    $geophp_objects = explode($this->configuration['delimiter'], $row->getSourceProperty($this->configuration['source']));
+
+    $value = array();
+    foreach ($geophp_objects as $geophp_object) {
+      $value[] = array(
+        'value' => $geophp_object,
+      );
+    }
+
+    return $value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function multiple() {
+    return TRUE;
+  }
+
+}
