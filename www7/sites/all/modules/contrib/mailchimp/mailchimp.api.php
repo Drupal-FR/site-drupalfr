d5f202124c56a405dd5a470c37e9c7718b51b70b
Florent Torregrosa <florent.torregrosa@gmail.com>
2016-11-11 15:33:50 +0100

    Update mailchimp to 7.x-4.7

--- www7/sites/all/modules/contrib/mailchimp/mailchimp.api.php
+++ www7/sites/all/modules/contrib/mailchimp/mailchimp.api.php
@@ -68,3 +68,43 @@ function hook_mailchimp_unsubscribe_user($list_id, $email) {
 function hook_mailchimp_api_key_alter(&$api_key, $context) {
 
 }
+
+/**
+ * Alter the entity options list on the automations entity form.
+ *
+ * @param array $entity_type_options
+ *   The full list of Drupal entities.
+ * @param string $automation_entity_label
+ *   The label for the automation entity, if it exists.
+ */
+function hook_mailchimp_automations_entity_options(&$entity_type_options, $automation_entity_label) {
+
+}
+
+/**
+ * Alter mergevars before a workflow automation is triggered.
+ *
+ * @param array $merge_vars
+ *   The merge vars that will be passed to MailChimp.
+ * @param object $automation_entity
+ *   The MailchimpAutomationEntity object.
+ * @param object $wrapped_entity
+ *   The EntityMetadataWrapper for the triggering entity.
+ */
+function hook_mailchimp_automations_mergevars_alter(&$merge_vars, $automation_entity, $wrapped_entity) {
+
+}
+
+/**
+ * Perform an action after a successful MailChimp workflow automation.
+ *
+ * @param object $automation_entity
+ *   The MailchimpAutomationEntity object.
+ * @param string $email
+ *   The email_property value from the MailchimpAutomationEntity.
+ * @param object $wrapped_entity
+ *   The EntityMetadataWrapper for the triggering entity.
+ */
+function hook_mailchimp_automations_workflow_email_triggered($automation_entity, $email, $wrapped_entity) {
+
+}
