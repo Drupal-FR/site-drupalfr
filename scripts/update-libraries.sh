7ec698fb16dc1b4484163669f7dbf3fac6964a71
Florent Torregrosa <florent.torregrosa@gmail.com>
2016-12-24 15:25:11 +0100

    Update contrib and use composer for devel libraries.

--- scripts/update-libraries.sh
+++ scripts/update-libraries.sh
@@ -15,18 +15,6 @@ LIBRARIES_TEMP=$(mktemp -d)
 # Create libraries directory in case it does not exist yet.
 mkdir -p $WWW_PATH/libraries
 
-if [ "${ENVIRONMENT_MODE}" = "dev" ]; then
-  # Highlight.js.
-  mkdir -p $WWW_PATH/libraries/highlight/styles
-  wget http://cdn.jsdelivr.net/highlight.js/$HIGHLIGHT_JS_VERSION/highlight.min.js       -O $WWW_PATH/libraries/highlight/highlight.pack.js
-  wget http://cdn.jsdelivr.net/highlight.js/$HIGHLIGHT_JS_VERSION/styles/default.min.css -O $WWW_PATH/libraries/highlight/styles/idea.css
-
-  # D3.js.
-  mkdir -p $WWW_PATH/libraries/d3
-  wget https://github.com/mbostock/d3/releases/download/v$D3_VERSION/d3.zip -P $LIBRARIES_TEMP
-  unzip -o $LIBRARIES_TEMP/d3.zip -d $WWW_PATH/libraries/d3
-fi
-
 # Dropzone.
 mkdir -p $WWW_PATH/libraries/dropzone
 wget https://github.com/enyo/dropzone/archive/v$DROPZONE_VERSION.zip                     -O $LIBRARIES_TEMP/dropzone.zip
