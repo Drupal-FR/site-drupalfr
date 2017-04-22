16f224a84ed62942df55e3a52380edff30b1e335
Florent Torregrosa <florent.torregrosa@gmail.com>
2017-04-22 17:24:02 +0200

    WIP on user menu.

---
+++ www/profiles/drupalfr/themes/drupalfr_theme/src/Plugin/Preprocess/MenuAccount.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\drupalfr_theme\Plugin\Preprocess;
+
+use Drupal\bootstrap\Plugin\Preprocess\PreprocessBase;
+use Drupal\bootstrap\Utility\Variables;
+use Drupal\Component\Render\FormattableMarkup;
+
+/**
+ * Implements hook_preprocess_menu__account().
+ *
+ * @ingroup plugins_preprocess
+ *
+ * @BootstrapPreprocess("menu__account")
+ */
+class MenuAccount extends PreprocessBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocessVariables(Variables $variables) {
+    // Add icon on user link.
+    if (isset($variables['items']['drupalfr_user.account.compte'])) {
+      $variables['items']['drupalfr_user.account.compte']['title'] = new FormattableMarkup(
+        '<span class="glyphicon glyphicon-user" aria-hidden="true"></span><span class="sr-only">@original_title</span>',
+        [
+          '@original_title' => $variables['items']['drupalfr_user.account.compte']['title'],
+        ]
+      );
+    }
+
+    parent::preprocessVariables($variables);
+  }
+
+}
