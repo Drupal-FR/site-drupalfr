c69e4d35df5e144727cc345d51c6d5301abd5e08
Florent Torregrosa <florent.torregrosa@gmail.com>
2017-04-30 11:23:35 +0200

    Add menu_link migration.

---
+++ www/profiles/drupalfr/modules/drupalfr_migrate/src/Plugin/migrate/source/MenuLink.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\drupalfr_migrate\Plugin\migrate\source;
+
+use Drupal\migrate_source_csv\Plugin\migrate\source\CSV;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for menu link.
+ *
+ * @MigrateSource(
+ *   id = "drupalfr_migrate_menu_link_csv"
+ * )
+ */
+class MenuLink extends CSV {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    if (!parent::prepareRow($row)) {
+      return FALSE;
+    }
+
+    // Prepare structure of options managed by menu_attributes.
+    $menu_attributes_options = [
+      'attributes' => [
+        'title',
+        'id',
+        'name',
+        'rel',
+        'class',
+        'style',
+        'target',
+        'accesskey',
+      ],
+      'item_attributes' => [
+        'id',
+        'class',
+        'style',
+      ],
+    ];
+
+    $menu_link_options = [];
+    foreach ($menu_attributes_options as $options_group_key => $options) {
+      $menu_link_options[$options_group_key] = [];
+      foreach ($options as $option) {
+        $menu_link_options[$options_group_key][$option] = $row->getSourceProperty($options_group_key . '_' . $option);
+      }
+    }
+
+    $row->setSourceProperty('options', $menu_link_options);
+
+    return TRUE;
+  }
+
+}
