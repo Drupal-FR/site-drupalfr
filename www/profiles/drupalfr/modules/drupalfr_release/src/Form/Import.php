64433a9eef390605ce37836b5d598db0532e7ba2
Florent Torregrosa <florent.torregrosa@gmail.com>
2017-07-16 11:33:50 +0200

    Rename module drupalfr_releases into drupalfr_release and node type drupal_release into release.

---
+++ www/profiles/drupalfr/modules/drupalfr_release/src/Form/Import.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\drupalfr_release\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\drupalfr_release\Service\ReleaseHelperInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class Imports for release xml.
+ *
+ * @package Drupal\drupalfr_release\Form
+ */
+class Import extends FormBase {
+
+  /**
+   * The release helper service.
+   *
+   * @var \Drupal\drupalfr_release\Service\ReleaseHelperInterface
+   */
+  protected $releaseHelper;
+
+  /**
+   * Constructs a ContentEntityForm object.
+   *
+   * @param \Drupal\drupalfr_release\Service\ReleaseHelperInterface $release_helper
+   *   The entity type manager.
+   */
+  public function __construct(ReleaseHelperInterface $release_helper) {
+    $this->releaseHelper = $release_helper;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('drupalfr_release.release_helper')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'drupalfr_release_import_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $release_config = $this->config('drupalfr_release.settings');
+
+    if (!empty($release_config->get('xml_address'))) {
+      $form['action']['import'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Import releases'),
+      ];
+    }
+    else {
+      $url = Url::fromRoute('drupalfr_release.settings');
+      if ($url->renderAccess($url->toRenderArray())) {
+        drupal_set_message($this->t('Please set the XML URL on <a href=":url">the settings page</a>.', [':url' => $url->toString()]), 'error');
+      }
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $batch = [
+      'title' => $this->t('Import releases'),
+      'operations' => [
+        [
+          '\Drupal\drupalfr_release\ReleaseBatchHelper::importReleaseListBatch',
+          [$this->releaseHelper->getFeedReleases()],
+        ],
+      ],
+      'finished' => '\Drupal\drupalfr_release\ReleaseBatchHelper::importReleaseBatchBatchFinished',
+    ];
+
+    batch_set($batch);
+  }
+
+}
