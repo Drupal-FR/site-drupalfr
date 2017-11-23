106dd1997edd7ee3f535c08461bf9ecf4466b05f
Floris Moriceau <floris.moriceau@komuneco.org>
2017-11-23 05:04:02 +0100

    Installing theme. No config given

---
+++ www/themes/custom/drupal_france/theme-settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Add custom theme settings to the ZURB Foundation sub-theme.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements hook_form_FORM_ID_alter().
+ * @param $form
+ * @param \Drupal\Core\Form\FormStateInterface $form_state
+ */
+function STARTER_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
+
+}
