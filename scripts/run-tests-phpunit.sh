99d956960a5d0dd330bb2c39f0ef8c8aa8c7ea75
Florent Torregrosa <florent.torregrosa@gmail.com>
2017-12-24 12:12:57 +0100

    No more need of .cache_activated.

--- scripts/run-tests-phpunit.sh
+++ scripts/run-tests-phpunit.sh
@@ -10,9 +10,6 @@ usage() {
 # Without drush alias, change temporarily directory to www.
 cd $WWW_PATH
 
-# Disable external cache.
-rm -f $WWW_PATH/sites/default/.cache_activated
-
 # Start Phantomjs in case of Javascript tests.
 $PROJECT_PATH/vendor/bin/phantomjs --ssl-protocol=any --ignore-ssl-errors=true $PROJECT_PATH/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
 
@@ -21,10 +18,6 @@ sudo -u $WEBSERVER_USER -E $PROJECT_PATH/vendor/bin/phpunit -c $PROJECT_PATH/scr
 
 # Stop Phantomjs process. A bit brutal, maybe there is a better way.
 pgrep phantomjs | xargs kill
-
-# Enable external cache.
-touch $WWW_PATH/sites/default/.cache_activated
-$DRUSH cache:rebuild
 
 # Back to the current directory.
 cd $CURRENT_PATH
