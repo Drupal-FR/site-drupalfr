e8bdd3794e9b0cdc23b321bbdcda7403e8961b0f
Florent Torregrosa <florent.torregrosa@gmail.com>
2018-01-13 18:53:25 +0100

    Rename company into organization to be able to include organism in the websites features.

--- scripts/tasks/migrate_imports.sh
+++ scripts/tasks/migrate_imports.sh
@@ -14,7 +14,7 @@ $DRUSH migrate:import drupalfr_website_sector --update
 $DRUSH migrate:import drupalfr_website_drupal_version --update
 $DRUSH migrate:import drupalfr_drupal_version --update
 $DRUSH migrate:import drupalfr_page --update
-$DRUSH migrate:import drupalfr_company --update
+$DRUSH migrate:import drupalfr_organization --update
 $DRUSH migrate:import drupalfr_job_offer --update
 $DRUSH migrate:import drupalfr_showcase --update
 $DRUSH migrate:import drupalfr_local_group --update
