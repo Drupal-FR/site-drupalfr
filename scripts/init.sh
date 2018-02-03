b0f5ba08550a299914f0e1878fafe06196dfde6a
Florent Torregrosa <florent.torregrosa@gmail.com>
2018-02-03 15:37:19 +0100

    More verbose scripts.

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,19 +2,19 @@
 
 . $(dirname ${BASH_SOURCE[0]})/script-parameters.sh
 
-# Copy example files.
+echo -e "${LIGHT_GREEN}Copy example files.${NC}"
 rsync -avz --ignore-existing $PROJECT_PATH/example.env                          $PROJECT_PATH/.env
 rsync -avz --ignore-existing $PROJECT_PATH/example.docker-compose.yml           $PROJECT_PATH/docker-compose.yml
 rsync -avz --ignore-existing $SCRIPTS_PATH/example.script-parameters.local.sh   $SCRIPTS_PATH/script-parameters.local.sh
 rsync -avz --ignore-existing $PROJECT_PATH/conf/drupal/default/example.settings.local.php $PROJECT_PATH/conf/drupal/default/settings.local.php
 
-# Create public files directory.
+echo -e "${LIGHT_GREEN}Create public files directory.${NC}"
 mkdir -p $WWW_PATH/sites/default/files
 
-# Ensure the following directories are present. Otherwise automated tests fail.
+echo -e "${LIGHT_GREEN}Ensure the following directories are present. Otherwise automated tests fail.${NC}"
 mkdir -p $WWW_PATH/modules
 mkdir -p $WWW_PATH/profiles
 mkdir -p $WWW_PATH/themes
 
-# Permissions are for dev environments. It should be less permissive.
+echo -e "${LIGHT_GREEN}Permissions are for dev environments. It should be less permissive.${NC}"
 chmod 777 $WWW_PATH/sites/default/files
--- scripts/update.sh
+++ scripts/update.sh
@@ -3,46 +3,42 @@
 . $(dirname ${BASH_SOURCE[0]})/script-parameters.sh
 . $(dirname ${BASH_SOURCE[0]})/script-parameters.local.sh
 
-# Without drush alias, change temporarily directory to www.
+echo -e "${LIGHT_GREEN}Without drush alias, change temporarily directory to www.${NC}"
 cd $WWW_PATH
 
-# Database backup.
+echo -e "${LIGHT_GREEN}Database backup.${NC}"
 $DRUSH sql:dump --result-file="${PROJECT_PATH}/backups/${CURRENT_DATE}.sql" --gzip --structure-tables-key="common"
 
-# Put the site in maintenance mode.
+echo -e "${LIGHT_GREEN}Put the site in maintenance mode.${NC}"
 $DRUSH state:set system.maintenance_mode 1
 
-# Install sources.
 . $SCRIPTS_PATH/tasks/composer_install.sh
 
-# Clear cache to be sure cache are cleared even if there is no update or Drush
-# has been updated.
+echo -e "${LIGHT_GREEN}Clear cache to be sure cache are cleared even if there is no update or Drush has been updated.${NC}"
 $DRUSH cache:rebuild
 
-# Launch updates. Ensure that the database schema is up-to-date.
+echo -e "${LIGHT_GREEN}Launch updates. Ensure that the database schema is up-to-date.${NC}"
 $DRUSH updatedb --entity-updates -y
 
-# Export prod config split in case of overrides.
+echo -e "${LIGHT_GREEN}Export prod config split in case of overrides.${NC}"
 $DRUSH config-split:export prod -y
 
-# Import configuration.
+echo -e "${LIGHT_GREEN}Import configuration.${NC}"
 $DRUSH config:import -y
 
-# Import content.
 # For update.sh import only content if the environment is dev to not risk
 # breaking prod.
 if [ "${ENVIRONMENT_MODE}" = "dev" ]; then
   . $SCRIPTS_PATH/tasks/migrate_imports.sh
 fi
 
-# Translation updates.
 . $SCRIPTS_PATH/tasks/update_translations.sh
 
-# Remove the maintenance mode.
+echo -e "${LIGHT_GREEN}Remove the maintenance mode.${NC}"
 $DRUSH state:set system.maintenance_mode 0
 
-# Run CRON.
+echo -e "${LIGHT_GREEN}Run CRON.${NC}"
 $DRUSH core:cron
 
-# Back to the current directory.
+echo -e "${LIGHT_GREEN}Back to the current directory.${NC}"
 cd $CURRENT_PATH
