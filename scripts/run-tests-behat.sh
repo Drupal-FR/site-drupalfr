f617e5baa4bb2253944f3171865b0cb37bbe65c4
Florent Torregrosa <florent.torregrosa@gmail.com>
2018-02-03 15:37:19 +0100

    More verbose scripts.

--- scripts/run-tests-behat.sh
+++ scripts/run-tests-behat.sh
@@ -3,5 +3,5 @@
 . $(dirname ${BASH_SOURCE[0]})/script-parameters.sh
 . $(dirname ${BASH_SOURCE[0]})/script-parameters.local.sh
 
-# Run behat tests.
+echo -e "${LIGHT_GREEN}Run behat tests.${NC}"
 $PROJECT_PATH/vendor/bin/behat --config=$SCRIPTS_PATH/tests/behat/behat.yml
--- scripts/run-tests-phpunit.sh
+++ scripts/run-tests-phpunit.sh
@@ -7,17 +7,17 @@ usage() {
 . $(dirname ${BASH_SOURCE[0]})/script-parameters.sh
 . $(dirname ${BASH_SOURCE[0]})/script-parameters.local.sh
 
-# Without drush alias, change temporarily directory to www.
+echo -e "${LIGHT_GREEN}Without drush alias, change temporarily directory to www.${NC}"
 cd $WWW_PATH
 
-# Start Phantomjs in case of Javascript tests.
+echo -e "${LIGHT_GREEN}Start Phantomjs in case of Javascript tests.${NC}"
 $PROJECT_PATH/vendor/bin/phantomjs --ssl-protocol=any --ignore-ssl-errors=true $PROJECT_PATH/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
 
-# Run tests.
+echo -e "${LIGHT_GREEN}Run tests.${NC}"
 sudo -u $WEBSERVER_USER -E $PROJECT_PATH/vendor/bin/phpunit -c $PROJECT_PATH/scripts/tests/phpunit.xml $@
 
-# Stop Phantomjs process. A bit brutal, maybe there is a better way.
+echo -e "${LIGHT_GREEN}Stop Phantomjs process. A bit brutal, maybe there is a better way.${NC}"
 pgrep phantomjs | xargs kill
 
-# Back to the current directory.
+echo -e "${LIGHT_GREEN}Back to the current directory.${NC}"
 cd $CURRENT_PATH
