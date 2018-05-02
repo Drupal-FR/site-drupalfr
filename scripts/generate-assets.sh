18256f840b0d9dc5b49cc165c5fd14b7410ec905
Florent Torregrosa <florent.torregrosa@gmail.com>
2018-05-02 09:22:29 +0200

    Add a script to generate assets.

---
+++ scripts/generate-assets.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# You should have the following tools installed globally:
+# - npm
+
+usage() {
+    echo "generate-assets.sh build|watch"
+}
+
+while getopts "h" opt; do
+    case "${opt}" in
+        h)
+            usage; exit 0;;
+        \?)
+            usage >&2; exit 1;;
+    esac
+done
+
+if [ -z "$1" ]
+then
+    ASSETS_MODE='build'
+else
+    ASSETS_MODE="$1"
+fi
+
+. $(dirname ${BASH_SOURCE[0]})/script-parameters.sh
+. $(dirname ${BASH_SOURCE[0]})/script-parameters.local.sh
+
+echo -e "${LIGHT_GREEN}Change temporarily directory.${NC}"
+cd $WWW_PATH/themes/custom/drupal_france
+
+echo -e "${LIGHT_GREEN}Generate assets with mode: ${ASSETS_MODE}${NC}"
+npm run-script $ASSETS_MODE
+
+echo -e "${LIGHT_GREEN}Back to the current directory.${NC}"
+cd $CURRENT_PATH
