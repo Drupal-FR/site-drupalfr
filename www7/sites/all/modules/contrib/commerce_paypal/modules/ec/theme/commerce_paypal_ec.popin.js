612034d95fe0bcaa0490121e022dc0a75ffa337f
Florent Torregrosa <florent.torregrosa@gmail.com>
2019-02-02 16:33:52 +0100

    Update commerce_paypal to 7.x-2.6

--- www7/sites/all/modules/contrib/commerce_paypal/modules/ec/theme/commerce_paypal_ec.popin.js
+++ www7/sites/all/modules/contrib/commerce_paypal/modules/ec/theme/commerce_paypal_ec.popin.js
@@ -9,7 +9,7 @@
       $('.paypal-bml-popin-text').hide();
       $('.paypal-bml-popin').click(function (event) {
         event.preventDefault();
-        $('.paypal-bml-popin-text').dialog({modal:true});
+        $('.paypal-bml-popin-text').dialog({modal:true, width: 550});
       });
     }
   };
